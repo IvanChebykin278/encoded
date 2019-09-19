d095e000679235f2b52c173d205bfb01a589728d
Chebykin Ivan <chebykin.i.v@gmail.com>
2019-09-19 17:41:35 +0500

    react-app second

--- src/store.js
+++ src/store.js
@@ -17,11 +17,7 @@ const middleware = [thunk];
 
 const store = createStore(
     rootReducer, 
-    initialState,
-    compose( 
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    initialState
 );
 
 export default store;
